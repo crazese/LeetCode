57ad112402bbe50ff98ab7756d8ea813b6f0d628
crazese <hustdwcz@gmail.com>
2016-03-07 00:16:10 +0800

    '#7'

---
+++ Reverse Integer.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x ==0 or abs(x)>2**31:return 0
+        sym = x/abs(x)
+        x= abs(x)
+        y = 0
+        while x >= 1:
+        	y = y*10 + x%10
+        	x = x/10
+        if y>2**31: return 0
+        y = y*sym
+        return y
+
+
+# test = Solution()
+# print test.reverse(1563847412)
+
