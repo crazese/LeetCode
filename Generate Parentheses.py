e6a7164dd2386e159ab08d63e49876c2c01b7118
crazese <hustdwcz@gmail.com>
2016-04-21 00:28:00 +0800

    #22

---
+++ Generate Parentheses.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        self.res = []
+        self.generateTemp(n,n,'',[])
+        print 'res',self.res
+        return self.res
+
+    def generateTemp(self,left, right, s, res):
+        if left == 0 and right ==0:
+            res.append(s)
+            self.res = res
+            return 
+
+        if left >0:
+            self.generateTemp(left-1,right,s+'(',res)
+
+        if left<right:
+            self.generateTemp(left,right-1,s+')',res)
+
+
+
+test = Solution()
+print test.generateParenthesis(3)
